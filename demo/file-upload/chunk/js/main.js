51b28b990b323024c96cee7c06a6eb1c22294368
wx1458084829 <15328400570@163.com>
2025-01-15 23:16:51 +0800

    添加注释

--- demo/file-upload/chunk/js/main.js
+++ demo/file-upload/chunk/js/main.js
@@ -3,17 +3,21 @@ import { cutFile } from './cutFile.js'; // 引入切片函数
 
 const inFile = document.querySelector('input[type="file"]');
 
-inFile.onchange =async (e) => {
+/**
+ * 监听文件上传
+ * @param {*} e 
+ */
+inFile.onchange = async (e) => {
 
-console.log(e.target.files[0]);
-const file = e.target.files[0];
-console.time('cutFile');
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    console.time('cutFile');
 
-const chunks = await cutFile(file)
+    const chunks = await cutFile(file)
 
-console.timeEnd('cutFile');
+    console.timeEnd('cutFile');
 
-// 得到切片后的文件
-console.log(chunks);
+    // 得到切片后的文件
+    console.log(chunks);
 
 }
